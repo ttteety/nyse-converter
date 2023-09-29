e9e2b2a4a1c14efb7f00d251d77024701026e5c3
ttteety <ttteety@yahoo.com>
2023-09-29 19:57:11 +1000

    Changed core logic to specify file names explicitly

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
 from dask import dataframe as dd
 import os 
+import glob
 
 def main():
     src_dir = os.environ['SRC_DIR']
-    tgt_dir = os.environ['TGT_DIR']
+    #tgt_dir = os.environ['TGT_DIR']
+    src_file_names = sorted(glob.glob(f'{src_dir}/NYSE*.txt.gz'))
+    tgt_file_names = [
+        file.replace('txt', 'json').replace('nyse_data', 'nyse_json') 
+        for file in src_file_names
+    ]
     print('File format conversion started')
     df = dd.read_csv(
-        f'{src_dir}/NYSE*.txt.gz',
+        src_file_names,
         names=['ticker', 'trade_date', 'open_price', 'low_price',
                'high_price', 'close_price', 'volume'],
         blocksize=None
     )
     print('Data Frame is created and will be written in JSON format')
     df.to_json(
-        f'{tgt_dir}/part-*.json.gz',
+        tgt_file_names,
         orient='records',
         lines=True,
         compression='gzip',
