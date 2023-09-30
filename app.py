bb7bbb299cddeb6a8bcdb8eecda7e4b5ff40b599
ttteety <ttteety@yahoo.com>
2023-09-30 10:43:49 +1000

    Added logging

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from dask import dataframe as dd
 import os 
 import glob
+import logging
 
 def main():
+    logging.basicConfig(
+        level=logging.INFO,
+        filename='logs/ffc.log',
+        format='%(levelname)s %(asctime)s %(message)s',
+        datefmt='%Y-%m-%d %I:%M:%S %p'
+    )
+    logging.info('File format conversion started')
     src_dir = os.environ['SRC_DIR']
     src_file_pattern = os.environ.setdefault('SRC_FILE_PATTERN', 'NYSE*.txt.gz')
     #tgt_dir = os.environ['TGT_DIR']
@@ -11,14 +19,13 @@ def main():
         file.replace('txt', 'json').replace('nyse_data', 'nyse_json') 
         for file in src_file_names
     ]
-    print('File format conversion started')
     df = dd.read_csv(
         src_file_names,
         names=['ticker', 'trade_date', 'open_price', 'low_price',
                'high_price', 'close_price', 'volume'],
         blocksize=None
     )
-    print('Data Frame is created and will be written in JSON format')
+    logging.info('Data Frame is created and will be written in JSON format')
     df.to_json(
         tgt_file_names,
         orient='records',
@@ -26,6 +33,7 @@ def main():
         compression='gzip',
         name_function=lambda i: '%05d' % i
     )
+    logging.info('File format conversion completed')
 
 if __name__ == "__main__":
     main()
